04a322025e114b79178b6dd0f170e634a3d52fc7
Akash-4089 <114065306+Akash-4089@users.noreply.github.com>
2022-10-07 13:41:44 +0530

    OUTPUT

--- Qn 7.c
+++ Qn 7.c
@@ -12,7 +12,7 @@ int shmid;
 shmid=shmget((key_t)2345, 1024, 0666|IPC_CREAT);   
 
 printf("Key of shared memory is %d\n",shmid);  
-shared_memory=shmat(shmid,NULL,0);   
+shared_memory=shmat(shmid,NULL,0);    
   
 printf("Process attached at %p\n",shared_memory);   
 
